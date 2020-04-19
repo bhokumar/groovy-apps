34b6a504be21cc96ce151743b6f2dc1ee5f4ae91
bhokumar <bhoopendra.akgec@gmail.com>
2020-04-19 14:45:50 +0530

    Adding Collector

---
+++ groovy-stream/src/main/groovy/com/github/bhokumar/streams/collectors/ApplicationFiltering.groovy
@@ -0,0 +1,39 @@
+package com.github.bhokumar.streams.collectors
+
+import com.github.bhokumar.streams.model.Gender
+import com.github.bhokumar.streams.model.Person
+
+import static java.util.stream.Collectors.*
+
+class ApplicationFiltering {
+
+    static List<Person> createPeople() {
+        Arrays.asList(
+                new Person('Sara', Gender.FEMALE, 20),
+                new Person('Sara', Gender.FEMALE, 22),
+                new Person('Bob', Gender.MALE, 20),
+                new Person('Paula', Gender.FEMALE, 32),
+                new Person('Paul', Gender.MALE, 32),
+                new Person('Jack', Gender.MALE, 2),
+                new Person('Jack', Gender.MALE, 72),
+                new Person('Jill', Gender.MALE, 42)
+        )
+    }
+
+    static void main(String[] args) {
+        println createPeople().parallelStream()
+            .collect(
+                    groupingBy(
+                            {it.age},
+                            mapping(
+                                    {it.name},
+                                    filtering(
+                                            {it.length() > 4},
+                                            toList()
+                                    )
+                            )
+                    )
+            )
+    }
+
+}
